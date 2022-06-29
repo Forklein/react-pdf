2cfa90d60e8cc879433cc6b5e1f2a817bc638766
Giuseppe <giuseppe.pisani@webora.it>
2022-06-29 10:02:30 +0200

    Add components ad fix css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import Loader from './components/Loader';
+
 function App() {
-
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
   
   return (
     <div className="App">
-      <Document file="/assets/pdf/lorem.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
+      <Loader/>
     </div>
   );
 }
